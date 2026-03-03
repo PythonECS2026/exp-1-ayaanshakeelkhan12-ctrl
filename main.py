a06095f60a74270689ac9e8d6716b4cdf16f555c
ayaanshakeelkhan12-ctrl <ayaanshakeelkhan12@gmail.com>
2026-03-03 20:53:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 # AIM: Create a personalized greeting message
-#      User enters name: eg: Shibu. and the program should print "Hello, Shibu" 
-# Coder: 
-# Date:
+
+# Coder: Tasneem Khan
+
+# Date: 5/1/26
 
 # Write your code here
+
+name=input("Enter Name: ")
+
+age=input("Enter Age: ")
+
+print("Welcome to Rizvi College of Engineering")
+
+print("Hello,", name)
+
+print("You are", age, "years old.")
+
